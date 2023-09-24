d3d3c8cfe2fa04ba9618466cab43b246550e2cf9
Satya Pandya <satyapandya@Satyas-MacBook-Pro.local>
2023-09-24 08:37:53 -0400

    update soli

--- scaffold-eth-2/packages/hardhat/contracts/Wander.sol
+++ scaffold-eth-2/packages/hardhat/contracts/Wander.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
 // Useful for debugging. Remove when deploying to a live network.
 //import "hardhat/console.sol";
