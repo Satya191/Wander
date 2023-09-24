bf3d90fd6d7f5e9808991914905b2d62434e6964
Gyges <shepherd2sovereign@gmail.com>
2023-09-23 23:50:30 -0400

    not complete but updates to payment-page

--- scaffold-eth-2/packages/nextjs/pages/payment-page.tsx
+++ scaffold-eth-2/packages/nextjs/pages/payment-page.tsx
@@ -3,17 +3,38 @@ import type { NextPage } from "next";
 // import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 // import { MetaHeader } from "~~/components/MetaHeader";
 // import { useAccount } from "wagmi";
-// import { Address } from '~~/components/scaffold-eth';
+import { Address } from '~~/components/scaffold-eth';
 import React, { useState } from 'react';
 
 // We first define the React component's props shape.
 interface MyFormProps {
-  onSubmit: (field1: string, field2: string) => void;
+  customButtonText: string;
 }
 
-const MyForm: React.FC<MyFormProps> = ({onSubmit }) => {
+const MyForm: React.FC<MyFormProps> = ({ customButtonText }) => {
   const [field1, setField1] = useState('');
   const [field2, setField2] = useState('');
+
+  // Here we call the Hook and set its values directly within `MyForm` component.
+  const { writeAsync, isLoading, isMining } = useScaffoldContractWrite({
+  // FIX
+  contractName:"Wander.sol",
+  functionName:"sendEther",
+  args: [field1],
+  value: field2, // This is IN ETH.
+  blockConfirmations:1,
+  onBlockConfirmation: (txnReceipt) => {
+    console.log("Transaction block hash ", txnReceipt.blockHash);
+  },
+});
+
+  // React automatically calls onSubmit whenever the form is submitted.
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!isLoading && !isMining) {
+      writeAsync();  // Call the asynchronous write function provided by `useScaffoldContractWrite`.
+    }
+  };
 
   return (
       <form className="flex flex-col items-center justify-center gap-3" onSubmit={(e) => {
@@ -28,7 +49,7 @@ const MyForm: React.FC<MyFormProps> = ({onSubmit }) => {
               <label className="text-center font-bold">Amount (in ETH)</label>
               <input className="input input-bordered w-full max-w-xs mb-7" type="text" value={field2} onChange={(e) => setField2(e.target.value)} />
           </div>
-          <button className="bg-primary btn btn-primary mt-5 mb-0" type="submit">Make payment!</button>
+          <button className="bg-primary btn btn-primary mt-5 mb-0" type="submit">{customButtonText}</button>
       </form>
   );
 };
@@ -43,7 +64,7 @@ const ParentComponent: NextPage = () => {
 
   return (
     <div>
-      <MyForm onSubmit={customSubmitFunction}/>
+      <MyForm customButtonText = "Make Payment!"/>
     </div>
   )
 };
